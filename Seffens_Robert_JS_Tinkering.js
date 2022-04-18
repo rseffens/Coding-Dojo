575b2fb19e913278b62ff58f803e3882c9f1f174
Robert Seffens <rseffens@gmail.com>
2022-04-18 13:10:56 -0700

    initial commit

---
+++ Seffens_Robert_JS_Tinkering.js
@@ -0,0 +1,10 @@
+function displayifChildIsAbleToRideTheRollerCoaster(){
+    for(var childHeight= 52)
+}
+if (childHeight < 52){
+    console.log("Get on the ride kiddo")
+}
+else{
+    console.log("Sorry kiddo. Maybe Next year")
+}
+displayifChildIsAbleToRideTheRollerCoaster
---
+++ Test.js
@@ -0,0 +1,5 @@
+function a(b,c)
+{
+    return b+c; 
+}
+console.log(a(a,1) + a(2,2))
